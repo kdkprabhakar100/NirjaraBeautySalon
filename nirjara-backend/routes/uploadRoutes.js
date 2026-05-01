148d251e22c40463687e01428ab763877ed2d637
kdkprabhakar100 <kdkprabhakar100@gmail.com>
2026-05-01 22:14:32 +0545

    setup cloudinary uploads

--- nirjara-backend/routes/uploadRoutes.js
+++ nirjara-backend/routes/uploadRoutes.js
@@ -1,36 +1,15 @@
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
-
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
-
-const upload = multer({
-  storage,
-  limits: { fileSize: 2 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    const allowed = /jpeg|jpg|png|webp/;
-    const ext = allowed.test(path.extname(file.originalname).toLowerCase());
-    const mime = allowed.test(file.mimetype);
-
-    if (ext && mime) cb(null, true);
-    else cb(new Error("Only images are allowed"));
-  },
-});
+const upload = require("../config/cloudinary");
 
 router.post("/", upload.single("image"), (req, res) => {
-  res.json({
-    imageUrl: `${process.env.BASE_URL}/uploads/${req.file.filename}`,
-  });
+  try {
+    res.json({
+      imageUrl: req.file.path,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Upload failed" });
+  }
 });
 
 module.exports = router;
